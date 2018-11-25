5382175be40bc72e4dfd6bdcc4be118a3cd60bf6
Don Stefani <don@fiftyflowers.com>
2018-11-25 11:12:23 -0700

    added airbnb eslint and formatter in jwt example

--- jwt/index.js
+++ jwt/index.js
@@ -1,25 +1,26 @@
-const express = require("express");
+const express = require('express');
 const bodyParser = require('body-parser');
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 const cors = require('cors');
-const app = express();
-const expressjwt = require("express-jwt");
-const PORT = 8888;
+const expressjwt = require('express-jwt');
 
-const jwtCheck = expressjwt({    
-  secret: "mykey"
+const app = express();
+const PORT = 8888;
+const jwtCheck = expressjwt({
+  secret: 'mykey',
 });
 
 // some data to use
 const users = [
-	{id: 1, username: 'clarkKent', password: 'superman'},
-	{id: 2, username: 'bruceWayne', password: 'batman'}
+  { id: 1, username: 'clarkKent', password: 'superman' },
+  { id: 2, username: 'bruceWayne', password: 'batman' },
 ];
 
 // tell express to use body-parser to handle all the JSON responses
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true,
 }));
+
 app.use(bodyParser.json());
 
 /**
@@ -32,42 +33,41 @@ app.use(cors());
 
 // sample route - GET the time - no JWT needed
 app.get('/time', (req, res) => {
-	const time = (new Date()).toLocaleTimeString();
-	res.status(200).send(`The Time is ${time}`);
+  const time = (new Date()).toLocaleTimeString();
+  res.status(200).send(`The Time is ${time}`);
 });
 
 /**
 you'll need to paste the JWT you get from login into
 the auth header "Bearer Token" to get to this route
 */
-app.get("/asset/secret", jwtCheck, (req, res) => {
-	res.status(200).send("Only logged in people can see me");
+app.get('/asset/secret', jwtCheck, (req, res) => {
+  res.status(200).send('Only logged in people can see me');
 });
 
 // route for 404 - put at the end of the get routes
-app.get("*", (req, res) => {
-	res.sendStatus(404);
+app.get('*', (req, res) => {
+  res.sendStatus(404);
 });
 
 // login route
 app.post('/login', (req, res) => {
-	if (!req.body.username || !req.body.password) {
-		res.status(400).send('Error. Please enter the coorect username and password');
-		return;
-	}
+  if (!req.body.username || !req.body.password) {
+    res.status(400).send('Error. Please enter the coorect username and password');
+    return;
+  }
 
-	const user = users.find((u) => {
-  		return u.username === req.body.username && u.password === req.body.password;
-	});
+  const user = users
+    .find(u => u.username === req.body.username && u.password === req.body.password);
 
-	// generate and send the JWT in reponse
-	const token = jwt.sign({
-	  sub: user.id,
-	  username: user.username
-	}, "mykey", {expiresIn: "3 hours"});
-	res.status(200).send({access_token: token});
+  // generate and send the JWT in reponse
+  const token = jwt.sign({
+    sub: user.id,
+    username: user.username,
+  }, 'mykey', { expiresIn: '3 hours' });
+  res.status(200).send({ access_token: token });
 });
 
 app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}.`);
+  console.log(`Server is running on port ${PORT}.`);
 });
